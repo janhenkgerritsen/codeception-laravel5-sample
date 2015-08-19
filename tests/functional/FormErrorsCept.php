1f7c01eeb61426250cb24fcb767a4adc799607fc
Jan-Henk Gerritsen <j.h.gerritsen@movage.nl>
2015-08-19 15:55:55 +0200

    Add tests for dontSeeFormErrors method

--- tests/functional/FormErrorsCept.php
+++ tests/functional/FormErrorsCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new FunctionalTester($scenario);
-$I->wantTo('test session errors');
+$I->wantTo('test form error assertions');
 
 $I->amOnPage('/auth/register');
 $I->click('button[type=submit]');
@@ -12,3 +12,11 @@ $I->seeFormErrorMessages(array(
     'name' => 'The name field is required.',
     'email' => 'The email field is required.'
 ));
+
+$I->fillField('name', 'John Doe');
+$I->fillField('email', 'johndoe@example.com');
+$I->fillField('password',  'password');
+$I->fillField('password_confirmation',  'password');
+$I->click('button[type=submit]');
+
+$I->dontSeeFormErrors();
