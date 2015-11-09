e57aad4d105cee4b7982b82cb9f12f06b46c173e
Jan-Henk Gerritsen <j.h.gerritsen@movage.nl>
2015-11-09 15:55:16 +0100

    Update tests for event functionality

--- tests/functional/EventsCest.php
+++ tests/functional/EventsCest.php
@@ -16,16 +16,41 @@ class EventsCest
         $I->seeRecord('users', ['email' => 'johndoe@example.com']);
     }
 
-    public function expectEvents(FunctionalTester $I)
+    public function seeEventTriggered(FunctionalTester $I)
     {
-        $I->expectEvents('App\Events\TestEvent');
         $I->amOnPage('fire-event');
+        $I->seeEventTriggered('App\Events\TestEvent');
     }
 
-    public function expectEventsWhenEventsAreDisabled(FunctionalTester $I)
+    public function seeEventTriggeredWithMultipleEvents(FunctionalTester $I)
+    {
+        $I->amOnPage('fire-event');
+        $I->seeEventTriggered('App\Events\TestEvent', 'kernel.handled');
+    }
+
+    public function seeEventTriggeredWithEventObject(FunctionalTester $I)
+    {
+        $I->amOnPage('fire-event');
+        $I->seeEventTriggered(new \App\Events\TestEvent());
+    }
+
+    public function seeEventTriggeredWhenEventsAreDisabled(FunctionalTester $I)
     {
         $I->disableEvents();
-        $I->expectEvents('App\Events\TestEvent');
+
         $I->amOnPage('fire-event');
+        $I->seeEventTriggered('App\Events\TestEvent');
+    }
+
+    public function dontSeeEventTriggered(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->dontSeeEventTriggered('App\Events\TestEvent');
+    }
+
+    public function dontSeeEventTriggeredWithEventObject(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->dontSeeEventTriggered(new \App\Events\TestEvent());
     }
 }
